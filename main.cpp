a484582f0ac6b2c3a01954fcf2a2a7e250e28510
jakob-a2k <jakub.a2k@gmail.com>
2022-11-17 21:15:25 +0100

    create new shuffle

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 string Color[4] = {"pik","kier","karo","trefl"};
 string Figure[6] = {"A","K","Q","W","10","9"};
 int Weight[6] = {6,5,4,3,2,1};
@@ -13,90 +12,70 @@ struct Card{
     string figure;
     int weight;
 };
-struct Player_One{
-    Card deck_one;
+struct Deck{
+    Card deck;
 };
-struct Player_Two{
-    Card deck_two;
-};
-void create_deck_of_cards(Card deck[]);
-bool is_in_pack(Card deck[],int a, int b,Player_One player_one[]);
-void passing_cards_out(Card deck[]);
-bool second_player_pack(Card deck[], Player_One player_one[], int i);
+void display(Card cards[]);
+void create_deck_of_cards(Card cards[]);
+void shuffle_cards(Card cards[]);
+bool rand_shuffle(Card cards[],Card temp[],int n, int t);
 
 int main()
 {
-    Card deck[24];
-    create_deck_of_cards(deck);
-    passing_cards_out(deck);
+    Card cards[24];
+    create_deck_of_cards(cards);
+    shuffle_cards(cards);
+    //display(cards);
 
     return 0;
 }
-void create_deck_of_cards(Card deck[]){
+void create_deck_of_cards(Card cards[]){
     int n = 0;
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 6; j++){
-            deck[n].sign = Color[i];
-            deck[n].figure = Figure[j];
-            deck[n].weight = Weight[j];
+            cards[n].sign = Color[i];
+            cards[n].figure = Figure[j];
+            cards[n].weight = Weight[j];
             n++;
         }
     }
 }
-void passing_cards_out(Card deck[]){
+void shuffle_cards(Card cards[]){
     srand(time(NULL));
-    Player_One player_one[12];
-    int i = 0;
-    while(i < 12){
-        int number = rand()%24;
-        if( !is_in_pack(deck,number,i,player_one)){
-            player_one[i].deck_one.sign = deck[number].sign;
-            player_one[i].deck_one.figure = deck[number].figure;
-            player_one[i].deck_one.weight = deck[number].weight;
-            i++;
-        }
-    }
-   /* for(int i = 0; i < 12; i++){
-        cout << player_one[i].deck_one.figure << " ";
-        cout << player_one[i].deck_one.sign << " ";
-        cout << player_one[i].deck_one.weight << endl;
-    */
-    Player_Two player_two[12];
     int total = 0;
-    while(total < 12){
-        for(int i = 0; i < 24; i++){
-            if(!second_player_pack(deck,player_one,i)){
-                player_two[total].deck_two.figure = deck[i].figure;
-                player_two[total].deck_two.sign = deck[i].sign;
-                player_two[total].deck_two.weight = deck[i].weight;
-                total++;
-            }
+    Card temp[24];
+    while(total < 24){
+        int number = rand()%24;
+        if(!rand_shuffle(cards,temp,number,total)){
+            temp[total].figure = cards[number].figure;
+            temp[total].sign = cards[number].sign;
+            temp[total].weight = cards[number].weight;
+            total++;
         }
     }
-   /* cout << endl;
-    for(int i = 0; i < 12; i++){
-        cout << player_two[i].deck_two.figure << " ";
-        cout << player_two[i].deck_two.sign << " ";
-        cout << player_two[i].deck_two.weight << endl;
-    }*/
-}
-bool is_in_pack(Card deck[],int a, int b, Player_One player_one[]){
-    for(int i = 0; i < b; i++){
-        if(player_one[i].deck_one.sign == deck[a].sign &&
-           player_one[i].deck_one.figure == deck[a].figure &&
-           player_one[i].deck_one.weight == deck[a].weight)
-           return true;
+    for(int i = 0; i < 24; i++){
+        cards[i].figure = temp[i].figure;
+        cards[i].sign = temp[i].sign;
+        cards[i].weight = temp[i].weight;
     }
-    return false;
 }
-
-bool second_player_pack(Card deck[], Player_One player_one[], int i){
-    for(int j = 0; j < 12; j++){
-        if(deck[i].figure == player_one[j].deck_one.figure &&
-            deck[i].sign == player_one[j].deck_one.sign &&
-            deck[i].weight == player_one[j].deck_one.weight)
+bool rand_shuffle(Card cards[],Card temp[],int n, int t){
+    for(int i = 0; i < t; i++){
+        if(temp[i].figure == cards[n].figure &&
+           temp[i].sign == cards[n].sign &&
+           temp[i].weight == cards[n].weight)
             return true;
     }
+
     return false;
 }
 
+
+
+void display(Card cards[]){
+    for(int y = 0; y < 24; y++){
+        cout << cards[y].figure << " ";
+        cout << cards[y].sign << " ";
+        cout << cards[y].weight << endl;
+    }
+}
